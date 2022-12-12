925f0392a4bab0a8797d3065dae12be8b15ba6c4
kunalmkv <kunalm4.kvtagore@gmail.com>
2022-12-12 18:04:47 +0530

    task9 done

--- dom3/main.js
+++ dom3/main.js
@@ -68,18 +68,21 @@ function filterItems(e) {
 
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 itemList.addEventListener('click', removeItem);
 form.addEventListener('submit', addItem);
+filter.addEventListener('keyup', filterItems);
 function addItem(e) {
     e.preventDefault();
-    var newItem = document.getElementById('item').value;
+    var newItem1 = document.getElementById('item').value;
+    var newItem2 = document.getElementById('item2').value;
     var li = document.createElement('li');
     li.className = 'list-group-item';
-    li.appendChild(document.createTextNode(newItem));
+    li.appendChild(document.createTextNode(newItem1 + " " + newItem2));
     itemList.appendChild(li);
     var deleteBtn = document.createElement('button');
     deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-    deleteBtn.appendChild(document.createTextNode('X'));
+    deleteBtn.appendChild(document.createTextNode('delete'));
     li.appendChild(deleteBtn);
 }
 
@@ -94,4 +97,15 @@ function removeItem(e) {
 function filterItems(e) {
     // convert text to lowercase
     var text = e.target.value.toLowerCase();
+    // Get lis
+    var items = itemList.getElementsByTagName('li');
+    // Convert to an array
+    Array.from(items).forEach(function (item) {
+        var itemName = item.firstChild.textContent;
+        if (itemName.toLowerCase().indexOf(text) != -1) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
 }
